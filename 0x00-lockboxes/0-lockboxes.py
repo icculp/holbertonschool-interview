a3cd2b942c7d5254057edf7840ceb8d2046b4d18
Ian Culp <icculp@gmail.com>
2020-12-08 01:23:14 +0000

    Adding files, incomplete

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Task 0 Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """ Determine if all the boxes can be opened """
+    b = set()
+    for box in boxes:
+        b.add(box)
+    print(b)
