63a74216f9568e4faf5d23c93f4c059db62f4690
Ian Culp <icculp@gmail.com>
2020-12-08 02:00:06 +0000

    Algorithm practice complete, waiting for checker

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,5 +8,21 @@ def canUnlockAll(boxes):
     """ Determine if all the boxes can be opened """
     b = set()
     for box in boxes:
-        b.add(box)
-    print(b)
+        for bo in box:
+            b.add(bo)
+    c = [i for i in range(1, len(boxes))]
+    c = set(c)
+    d = c.issubset(b)
+    if d:
+        for i in range(1, len(boxes)):
+            temp = set()
+            for j in range(len(boxes)):
+                if i == j:
+                    continue
+                for bo in boxes[j]:
+                    temp.add(bo)
+            if i not in temp:
+                return False
+        return True
+    else:
+        return False
--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -10,3 +10,6 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+boxes = [[1,3],[3,0,1],[2],[0]]
+print(canUnlockAll(boxes))
