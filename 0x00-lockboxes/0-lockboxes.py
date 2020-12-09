3549f4f64fcee8a2ef97f2078d491865060ec814
Ian Culp <icculp@gmail.com>
2020-12-09 23:57:42 +0000

    Recursive solution works better

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,6 +6,23 @@
 
 def canUnlockAll(boxes):
     """ Determine if all the boxes can be opened """
+    length = len(boxes)
+    open = [0] * length
+
+    def depth_first(i):
+        """ recursive depth-first search """
+        for box in boxes[i]:
+            if not open[box]:
+                open[box] = 1
+                depth_first(box)
+        open[i] = 1
+    depth_first(0)
+    s = sum(open)
+    if s == length:
+        return True
+    return False
+
+    '''
     b = set()
     if type(boxes) is not list:
         return False
@@ -24,14 +41,23 @@ def canUnlockAll(boxes):
                     continue
                 for bo in boxes[j]:
                     temp.add(bo)
-                if j in boxes[i]:
+                for ki in boxes[i]:
+                    if i in boxes[ki]:
+                        if i not in temp:
+                            break
+                if i in boxes[]:
                     continue
                 for bo in boxes[j]:
                     temp2.add(bo)
-            if i not in temp2 or i not in temp:
+            if i not in temp:
+                print('false, i: {}'.format(i))
+                if i in temp2:
+                    print('true, i: {}'.format(i))
+                    return True
                 return False
-            '''if i in boxes[boxes[i].first]:
-                return False'''
+            """if i in boxes[boxes[i].first]:
+                return False"""
         return True
     else:
         return False
+    '''
