4157d5ee82decbdca81921f6e5eae3b908e7571b
Ian Culp <icculp@gmail.com>
2020-12-15 16:22:31 +0000

    First stab at sorted insertion

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+* insert_node - Inserts a node into a singly linked list
+* @head: Head of linked list
+* @number: Number to insert
+* Return: Pointer to node
+*/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp, *next;
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	if (head == NULL)
+		return (NULL);
+	temp = *head;
+	if (number < temp->n)
+	{
+		new->next = temp;
+		*head = new;
+		temp = NULL;
+	}
+	while (temp)
+	{
+		next = temp->next;
+		if (number < next->n)
+		{
+			temp->next = new;
+			new->next = next;
+			return (new);
+		}
+		temp = temp->next;
+	}
+	return (new);
+}
