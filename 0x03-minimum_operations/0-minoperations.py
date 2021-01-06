58fb8b692211c5816f6c7002423018d5af6efe59
Ian Culp <icculp@gmail.com>
2021-01-06 20:02:19 +0000

    negatives getting counted

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,7 +6,7 @@
 
 def minOperations(n):
     ''' Min operations of fib (sum of previous 2) to add to n '''
-    if n == 0 or n == 1 or type(n) is not int:
+    if n < 2 or type(n) is not int:
         return 0
     count = 0
     while n % 2 == 0:
