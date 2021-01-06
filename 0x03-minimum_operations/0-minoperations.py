60b674bc52c810354604def5d049f94e6e042067
Ian Culp <icculp@gmail.com>
2021-01-06 18:22:38 +0000

    0 or 1

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,7 +7,9 @@ import math
 
 def minOperations(n):
     """ Min operations of fib (sum of previous 2) to add to n """
-    if n < 5 and n > 0:
+    if n == 0 or n == 1:
+        return 0
+    if n < 5 and n > 1:
         return n
     try:
         s = math.sqrt(n)
