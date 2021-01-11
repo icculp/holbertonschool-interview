b479acfc91925faed1477bd616ee3b294f80bbfe
Ian Culp <icculp@gmail.com>
2021-01-11 21:22:08 +0000

    Sandpiles skeleton

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,13 @@
+#include "sandpiles.h"
+
+/**
+* sandpiles_sum - Computes sum of two sandpiles
+* @grid1: First grid 3x3 grid (0-3)
+* @grid2: Second 3x3 grid (0-3)
+*/
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+
+
+}
