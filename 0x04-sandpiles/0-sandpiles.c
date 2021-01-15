e7ca37784361311daccf9335f9f4a7e8e94dd556
Ian Culp <icculp@gmail.com>
2021-01-15 04:32:04 +0000

    Unfinished but compiling

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,5 +1,29 @@
 #include "sandpiles.h"
 
+
+/**
+* print_grid - prints a 3x3 grid
+* @grid: the grid
+*/
+
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+
 /**
 * sandpiles_sum - Computes sum of two sandpiles
 * @grid1: First grid 3x3 grid (0-3)
@@ -8,6 +32,10 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	int i, j;
 
-
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
+	print_grid(grid1);
 }
