25c0ed80e8dc5d714b2269df0b6997f0b1e9e491
Ian Culp <icculp@gmail.com>
2021-01-27 22:32:32 +0000

    Possibly working version

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
-from signal import signal, SIGINT, SIG_IGN, getsignal
-from _thread import start_new_thread
+from signal import signal, SIGINT
+
 
 class Log:
     """ holds important attributes for loging """
@@ -20,32 +20,17 @@ class Log:
 
     def do_stuff(self):
         """ do stuff """
-        print("INSIDE")
         i = 0
-        try:
-            print('inside try')
-            sys.stdin.flush()
-            for line in sys.stdin:
-                print('----------------------')
-                #print(line)
-                #print('----------------------')
-                #print_dat(cache, size)
-                #print(line)
-                ls = line.split(' ')
-                code = ls[7]
-                self.cache[code] += 1
-                self.size += eval(ls[8].strip('\n'))
-                self.i += 1
-                if self.i == 10:
-                    self.i = 0
-                    self.print_dat()
-                    self.cache = self.empty_cache.copy()
-                #print(code)
-            print('end of try')
-        except KeyboardInterrupt:
-            print('weeeeee')
-            sys.stdin.flush()
-            sys.stdout.flush()
+        for line in sys.stdin:
+            ls = line.split(' ')
+            code = ls[7]
+            self.cache[code] += 1
+            self.size += eval(ls[8].strip('\n'))
+            self.i += 1
+            if self.i == 10:
+                self.i = 0
+                self.print_dat()
+                self.cache = self.empty_cache.copy()
 
     def print_dat(self):
         """ Prints dat cache """
@@ -55,29 +40,15 @@ class Log:
                 print("{}: {}".format(k, self.cache[k]))
         self.cache = self.empty_cache.copy()
 
-log = Log()
 
 def handler(signum, frame):
     """ handles ctrl-c signal """
-    print('SIGINT or CTRL-C detected. Exiting gracefully')
-    # restore the original signal handler as otherwise evil things will happen
-    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
-    signal(SIGINT, SIG_IGN)
-    sys.stdout.flush()
-    pass
+    log.print_dat()
+    exit()
+
 
 if __name__ == '__main__':
     log = Log()
-    #signal(SIGINT, SIG_IGN)
+    signal(SIGINT, handler)
     while(1):
-        #original_sigint = getsignal(SIGINT)
-        #signal(SIGINT, SIG_IGN)
-        #pid = os.fork()
         log.do_stuff()
-        print('here?')
-        #sys.stdin.flush()
-        #sys.stdout.flush()
-        #except KeyboardInterrupt:
-            #sys.stdin.close
-            #log.do_stuff()
-        #    pass
