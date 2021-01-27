d898d21301371f752bb5b7b4603387486287e193
Ian Culp <icculp@gmail.com>
2021-01-27 22:26:46 +0000

    Current log parser

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,56 +1,83 @@
 #!/usr/bin/python3
 import sys
-from signal import signal, SIGINT
+from signal import signal, SIGINT, SIG_IGN, getsignal
+from _thread import start_new_thread
 
+class Log:
+    """ holds important attributes for loging """
 
+    def __init__(self):
+        """ init """
+        empty_cache = {}
+        li = [200, 301, 400, 401, 403, 404, 405, 500]
+        for l in li:
+            empty_cache[str(l)] = 0
+        self.empty_cache = empty_cache
+        self.id = 1
+        self.cache = empty_cache.copy()
+        self.size = 0
+        self.i = 0
 
-def print_dat(cache, size, empty_cache):
-    """ Prints dat cache """
-    print("File size: {}".format(size))
-    for k in sorted(cache.keys()):
-        if cache[k] != 0:
-            print("{}: {}".format(k, cache[k]))
-    cache = empty_cache.copy()
+    def do_stuff(self):
+        """ do stuff """
+        print("INSIDE")
+        i = 0
+        try:
+            print('inside try')
+            sys.stdin.flush()
+            for line in sys.stdin:
+                print('----------------------')
+                #print(line)
+                #print('----------------------')
+                #print_dat(cache, size)
+                #print(line)
+                ls = line.split(' ')
+                code = ls[7]
+                self.cache[code] += 1
+                self.size += eval(ls[8].strip('\n'))
+                self.i += 1
+                if self.i == 10:
+                    self.i = 0
+                    self.print_dat()
+                    self.cache = self.empty_cache.copy()
+                #print(code)
+            print('end of try')
+        except KeyboardInterrupt:
+            print('weeeeee')
+            sys.stdin.flush()
+            sys.stdout.flush()
 
+    def print_dat(self):
+        """ Prints dat cache """
+        print("File size: {}".format(self.size))
+        for k in sorted(self.cache.keys()):
+            if self.cache[k] != 0:
+                print("{}: {}".format(k, self.cache[k]))
+        self.cache = self.empty_cache.copy()
 
-def handler(signal_received, frame):
-    ''' Handle signal '''
-    #print('SIGINT or CTRL-C detected. Exiting gracefully')
-    #print(frame)
-    print_dat(cache, size)
+log = Log()
 
-signal(SIGINT, handler)
-i = 0
-size = 0
-def do_stuff(size):
-    """ does the stuff """
-    empty_cache = {}
-    li = [200, 301, 400, 401, 403, 404, 405, 500]
-    for l in li:
-        empty_cache[str(l)] = 0
-    cache = empty_cache.copy()
-    i = 0
-    for line in sys.stdin:
-        #print('----------------------')
-        #print(line)
-        #print('----------------------')
-        #print_dat(cache, size)
-        #print(line)
-        ls = line.split(' ')
-        code = ls[7]
-        cache[code] += 1
-        size += eval(ls[8].strip('\n'))
-        i += 1
-        if i == 10:
-            i = 0
-            print_dat(cache, size, empty_cache)
-            cache = empty_cache.copy()
-        #print(code)
-def handler(signal_received, frame):
-    # Handle any cleanup here
+def handler(signum, frame):
+    """ handles ctrl-c signal """
     print('SIGINT or CTRL-C detected. Exiting gracefully')
-    print(frame)
-    print_dat(cache, size)
-signal(SIGINT, handler)
+    # restore the original signal handler as otherwise evil things will happen
+    # in raw_input when CTRL+C is pressed, and our signal handler is not re-entrant
+    signal(SIGINT, SIG_IGN)
+    sys.stdout.flush()
+    pass
 
-do_stuff(size)
+if __name__ == '__main__':
+    log = Log()
+    #signal(SIGINT, SIG_IGN)
+    while(1):
+        #original_sigint = getsignal(SIGINT)
+        #signal(SIGINT, SIG_IGN)
+        #pid = os.fork()
+        log.do_stuff()
+        print('here?')
+        #sys.stdin.flush()
+        #sys.stdout.flush()
+        #except KeyboardInterrupt:
+            #sys.stdin.close
+            #log.do_stuff()
+        #    pass
