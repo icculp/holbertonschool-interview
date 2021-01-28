b942ba545af0f5a1de93e92de62105a6a6552141
Ian Culp <icculp@gmail.com>
2021-01-28 02:17:14 +0000

    Trying inside main guard

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,57 +2,58 @@
 import sys
 
 
-class Log:
-    """ holds important attributes for loging """
+if __name__ == '__main__':
+    class Log:
+        """ holds important attributes for loging """
 
-    def __init__(self):
-        """ init """
-        empty_cache = {}
-        li = [200, 301, 400, 401, 403, 404, 405, 500]
-        for l in li:
-            empty_cache[str(l)] = 0
-        self.empty_cache = empty_cache.copy()
-        self.id = 1
-        self.cache = empty_cache.copy()
-        self.size = 0
-        self.i = 0
+        def __init__(self):
+            """ init """
+            empty_cache = {}
+            li = [200, 301, 400, 401, 403, 404, 405, 500]
+            for l in li:
+                empty_cache[str(l)] = 0
+            self.empty_cache = empty_cache.copy()
+            self.id = 1
+            self.cache = empty_cache.copy()
+            self.size = 0
+            self.i = 0
 
-    def do_stuff(self):
-        """ do stuff """
-        i = 0
-        for line in sys.stdin:
-            ls = line.split(' ')
-            code = ls[7]
-            '''print("code: [{}]".format(code))
-            print("cachecode1: [{}]".format(self.cache[code]))'''
-            '''if self.cache.get(code) is None:
-                self.cache.update({code: 1})
-            else:'''
-            self.cache[code] += 1
-            '''self.cache[code] += 1'''
-            '''self.cache['cool'] += 2'''
-            '''print("cachecode2: [{}]".format(self.cache[code]))
-            print("--------")'''
-            self.size += eval(ls[8].strip('\n'))
-            self.i += 1
-            if self.i == 10:
-                self.i = 0
-                self.print_dat()
-                '''self.cache = self.empty_cache.copy()'''
+        def do_stuff(self):
+            """ do stuff """
+            i = 0
+            for line in sys.stdin:
+                ls = line.split(' ')
+                code = ls[7]
+                '''print("code: [{}]".format(code))
+                print("cachecode1: [{}]".format(self.cache[code]))'''
+                '''if self.cache.get(code) is None:
+                    self.cache.update({code: 1})
+                else:'''
+                self.cache[code] += 1
+                '''self.cache[code] += 1'''
+                '''self.cache['cool'] += 2'''
+                '''print("cachecode2: [{}]".format(self.cache[code]))
+                print("--------")'''
+                self.size += eval(ls[8].strip('\n'))
+                self.i += 1
+                if self.i == 10:
+                    self.i = 0
+                    self.print_dat()
+                    '''self.cache = self.empty_cache.copy()'''
 
-    def print_dat(self):
-        """ Prints dat cache """
-        print("File size: {:d}".format(self.size))
-        for k in sorted(self.cache.keys()):
-            if self.cache[k] != 0:
-                print("{}: {}".format(k, self.cache[k]))
-        '''self.cache = self.empty_cache.copy()'''
+        def print_dat(self):
+            """ Prints dat cache """
+            print("File size: {:d}".format(self.size))
+            for k in sorted(self.cache.keys()):
+                if self.cache[k] != 0:
+                    print("{}: {}".format(k, self.cache[k]))
+            '''self.cache = self.empty_cache.copy()'''
 
 
-log = Log()
-try:
-    log.do_stuff()
-    log.print_dat()
-except KeyboardInterrupt:
-    log.print_dat()
-    raise
+    log = Log()
+    try:
+        log.do_stuff()
+        log.print_dat()
+    except KeyboardInterrupt:
+        log.print_dat()
+        raise
