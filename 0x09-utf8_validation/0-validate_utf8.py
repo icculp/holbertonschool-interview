d9bed345fc29e421a88776578e9e91c3c85aa20c
Ian Culp <icculp@gmail.com>
2021-02-15 20:51:07 +0000

    Simple challenge completed

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Utf-8 - 8 least significant bit encoding
+"""
+
+
+def validUTF8(data):
+    """ Parses data in list for valid 1 byte utf8 encoding
+        True if valid else False
+    """
+    if (type(data) is not list or
+        not all(isinstance(d, int) for d in data) or
+            not all(d >= 0 and d <= 128 for d in data)):
+        return False
+    else:
+        return True
