13718bcbdea2b6162cfc9e76fd043dee9d5ddc52
Ian Culp <icculp@gmail.com>
2021-02-17 23:24:33 +0000

    Checking if missing values important

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -38,7 +38,7 @@ def validUTF8(data):
                     if not (data[d + i] >= 128 and data[d + i] <= 191):
                         return False
                 d = d + i
-            elif data[d] >= 194 and data[d] <= 223:
+            elif data[d] >= 192 and data[d] <= 223:
                 for i in range(1, 2):
                     if not (data[d + i] >= 128 and data[d + i] <= 191):
                         return False
