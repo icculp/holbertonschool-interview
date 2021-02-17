4e7f1ef377fb2dd4017e90006602e26e0f887664
Ian Culp <icculp@gmail.com>
2021-02-17 20:48:12 +0000

    Pycodestyle

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -18,11 +18,11 @@ def validUTF8(data):
                     for d in last_bytes)):
         return False'''
     if (type(data) is not list or
-        not all(isinstance(d, int) for d in data)):
+            not all(isinstance(d, int) for d in data)):
         return False
     if not all((d >= 0 and d <= 127) or
-                (d >= 194 and d <= 244)
-                for d in data):
+               (d >= 194 and d <= 244)
+               for d in data):
         return False
     else:
         return True
