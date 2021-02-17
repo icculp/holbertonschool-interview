e3ec6711a0729f7a948645e044e59e1b12522850
Ian Culp <icculp@gmail.com>
2021-02-17 23:21:07 +0000

    Applying mask

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,9 +8,9 @@ def validUTF8(data):
     """ Parses data in list for valid 1 byte utf8 encoding
         True if valid else False
     """
-    '''last_byte_mask = 255'''
+    last_byte_mask = 255
     '''data = [257, 525, 410]'''
-    '''last_bytes = [d & last_byte_mask for d in data]'''
+    data = [d & last_byte_mask for d in data]
     '''print(last_bytes)'''
     '''if (type(data) is not list or
         not all(isinstance(d, int) for d in data) or
