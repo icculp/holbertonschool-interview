ff8580050218edda29bbeecf02de755dfeecdc9f
Ian Culp <icculp@gmail.com>
2021-02-25 03:20:21 +0000

    Sliding function complete

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,13 +1,85 @@
 #include "slide_line.h"
 
 /**
-* slide_line - Reproduce 2048 game on one line
+* left - Reproduce 2048 game on one line, no memory allocation
 * @line: Array of ints containing size elements
-* @
+* @size: number of elements to slide
+*/
+
+void left(int *line, size_t size)
+{
+	size_t i, j = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		while (line[j] == 0)
+			j++;
+		if (j > size)
+			break;
+		line[i] = line[j];
+		if (i != j)
+			line[j] = 0;
+		j++;
+	}
+}
+
+/**
+* right - Reproduce 2048 game on one line, no memory allocation
+* @line: Array of ints containing size elements
+* @size: number of elements to slide
+*/
+
+void right(int *line, size_t size)
+{
+	int i, j = 0;
+
+	for (i = j = size - 1; i >= 0; i--)
+	{
+		while (line[j] == 0)
+			j--;
+		if (j < 0)
+			break;
+		line[i] = line[j];
+		if (i != j)
+			line[j] = 0;
+		j--;
+	}
+}
+
+/**
+* merge - Reproduce 2048 game on one line, no memory allocation
+* @line: Array of ints containing size elements
+* @size: number of elements to slide
+*/
+
+void merge(int *line, size_t size)
+{
+	size_t i = size;
+
+	(void)line;
+	(void)i;
+	for (i = 1; i < size; i++)
+	{
+		if (line[i - 1] == line[i])
+			line[i - 1] = line[i - 1] * line[i], line[i] = 0;
+	}
+}
+
+/**
+* slide_line - Reproduce 2048 game on one line, no memory allocation
+* @line: Array of ints containing size elements
+* @size: number of elements to slide
+* @direction: direction to slide (left or right, otherwise fail)
+* Return: 1 upon success, 0 on failure
 */
 
 int slide_line(int *line, size_t size, int direction)
 {
-
-
+	if (direction == SLIDE_LEFT)
+		left(line, size), merge(line, size), left(line, size);
+	else if (direction == SLIDE_RIGHT)
+		right(line, size), merge(line, size), right(line, size);
+	else
+		return (0);
+	return (1);
 }
--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,9 +1,9 @@
 #ifndef SLIDE_LEFT
-#define SLIDE_LEFT():
+#define SLIDE_LEFT 0
 #endif
 
 #ifndef SLIDE_RIGHT
-#define SLIDE_RIGHT():
+#define SLIDE_RIGHT 1
 #endif
 
 #ifndef SLIDE_H
@@ -12,6 +12,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void left(int *line, size_t size);
+void right(int *line, size_t size);
 int slide_line(int *line, size_t size, int direction);
 
 #endif
