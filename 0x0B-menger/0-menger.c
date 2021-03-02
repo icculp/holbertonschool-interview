413c687404654a9d20d1f22395eefb81cbe6fb59
Ian Culp <icculp@gmail.com>
2021-03-02 21:49:19 +0000

    Sierpinsky carpet

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,5 +1,7 @@
 #include "menger.h"
 
+
+
 /**
 * menger - draws a 2d menger sponge
 * @level: level of menger sponge to draw (do nothing < 0)
@@ -7,6 +9,34 @@
 
 void menger(int level)
 {
+	int side = pow(3, level), i, j, d, dim;
+
 	if (level < 0)
 		return;
+	if (level == 0)
+	{
+		printf("#\n");
+		return;
+	}
+	for (i = 0, dim = 1; i < level; i++, dim *= 3)
+		;
+	for (i = 0; i < side; i++)
+	{
+		for (j = 0; j < side; j++)
+		{
+			for (d = dim / 3; d; d /= 3)
+			{
+				if ((i % (d * 3)) / d == 1 && (j % (d * 3)) / d == 1)
+					break;
+			}
+			if (d)
+			{
+				printf(" ");
+			}
+			else
+				printf("#");
+		}
+		printf("\n");
+
+	}
 }
