4f2c477b5aaf8674a1ee05616391d896aaea9c8f
Ian Culp <icculp@gmail.com>
2021-03-02 16:53:55 +0000

    Menger sponge skeleton

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,12 @@
+#include "menger.h"
+
+/**
+* menger - draws a 2d menger sponge
+* @level: level of menger sponge to draw (do nothing < 0)
+*/
+
+void menger(int level)
+{
+	if (level < 0)
+		return;
+}
