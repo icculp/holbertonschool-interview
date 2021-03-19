4e5856ad43fd38ed209ec24f6bcc60daaf7eb7f2
Ian Culp <icculp@gmail.com>
2021-03-19 03:11:04 +0000

    N queens solution

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -5,8 +5,6 @@
 import sys
 
 
-'''print(len(sys.argv))
-print(sys.argv)'''
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
@@ -16,5 +14,35 @@ if not N.isdigit():
     exit(1)
 N = int(N)
 if N < 4:
-    print(" N must be at least 4")
+    print("N must be at least 4")
+    exit(1)
 
+
+def solution(n):
+    '''
+        N Queens solver
+    '''
+    sol = [[i] for i in range(n)]
+    for i in range(1, n):
+        new_sol = []
+        for x in sol:
+            idx = [j for j in range(n) if j not in x]
+            for k in range(len(x)):
+                a, b = x[k] - i + k, x[k] + i - k
+                if a in idx:
+                    idx.remove(a)
+                if b in idx:
+                    idx.remove(b)
+                if idx == []:
+                    break
+            new_sol.extend([x + [j] for j in idx])
+        sol = new_sol
+    for s in range(len(sol)):
+        for i in range(n):
+            sol[s][i] = [i, sol[s][i]]
+    return sol
+
+
+if __name__ == '__main__':
+    for sol in solution(N):
+        print(sol)
