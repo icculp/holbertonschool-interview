8574f0955676c886c5501526ae77faee95d986c4
Ian Culp <icculp@gmail.com>
2021-03-19 03:18:15 +0000

    Moving conditionals to name guard

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -5,19 +5,6 @@
 import sys
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-N = sys.argv[1]
-if not N.isdigit():
-    print("N must be a number")
-    exit(1)
-N = int(N)
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
-
-
 def solution(n):
     '''
         N Queens solver
@@ -44,5 +31,17 @@ def solution(n):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    N = sys.argv[1]
+    if not N.isdigit():
+        print("N must be a number")
+        exit(1)
+    N = int(N)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
     for sol in solution(N):
         print(sol)
