5208f274a8595e3d0685cc83a9dee17e7795fd76
Ian Culp <icculp@gmail.com>
2021-03-26 02:53:58 +0000

    Balanced BST (but calling an AVL) complete

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,59 @@
+#include "binary_trees.h"
+
+/**
+* newNode - creates new node
+* @data: value to add to node
+* Return: node pointer
+*/
+avl_t *newNode(int data)
+{
+	avl_t *node;
+
+	node = malloc(sizeof(avl_t));
+	node->n = data;
+	node->parent = NULL;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
+
+/**
+* sortedArrayToBST - Inserts reursivley to BST
+* @arr: Array slice to insert from
+* @parent: Parent node to attach
+* @start: start of slice
+* @end: end of slice
+* Return: root of tree
+*/
+avl_t *sortedArrayToBST(int arr[], avl_t *parent, int start, int end)
+{
+	avl_t *root;
+	int mid;
+
+	if (start > end)
+		return (NULL);
+	mid = (start + end) / 2;
+	root = newNode(arr[mid]);
+	root->parent = parent;
+	root->left =  sortedArrayToBST(arr, root, start, mid - 1);
+	root->right = sortedArrayToBST(arr, root, mid + 1, end);
+	return (root);
+}
+
+/**
+* sorted_array_to_avl - Converts sorted array to AVL tree
+* @array: Sorted array to convert
+* @size: Size of the tree
+* Return: Pointer to root, or NULL on fail
+*/
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *root = NULL;
+
+	if (array == NULL)
+		return (NULL);
+
+	root = sortedArrayToBST(array, root, 0, (int)size - 1);
+	return (root);
+}
