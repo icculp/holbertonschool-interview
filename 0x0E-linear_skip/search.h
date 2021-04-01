067fc8497434f2babc38d35402aec0516f312d5d
Ian Culp <icculp@gmail.com>
2021-04-01 20:25:32 +0000

    betty on header

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -20,14 +20,14 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 
- void init_express(skiplist_t *list, size_t size);
+void init_express(skiplist_t *list, size_t size);
 
 skiplist_t *create_skiplist(int *array, size_t size);
 void print_skiplist(const skiplist_t *list);
