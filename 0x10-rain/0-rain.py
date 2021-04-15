eb9251170be000dc69091c84f5c845ac68907687
Ian Culp <icculp@gmail.com>
2021-04-15 01:41:09 +0000

    Slightly more complex version, stab 1

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -14,11 +14,37 @@ def rain(walls):
     """
     if len(walls) < 2:
         return 0
+    i = j = c = z = 0
+    while 1:
+        z = 0
+        if walls[i] != 0:
+            a = walls[i]
+            j = i + 1
+            while walls[j] == 0:
+                z += 1
+                j += 1
+                if j >= len(walls):
+                    return c
+            i = j
+            c += z * min(walls[i], walls[j])
+        i += 1
+        if i >= len(walls):
+            return c
+    ''' didn't work
     w = set([x for x in walls if x != 0])
-    c = 0
+    m = min(w)
+    z = walls[1:-1].count(0)
+    c = m * z
+    '''
+    ''' 3 missing checks
+    w = set([x for x in walls if x != 0])
     while len(w) > 1:
         c += max(w) - min(w)
         w.remove(min(w))
-    # for x in range(len(w) - 1):
-    #     c += min(w[x], w[x + 1])
+    '''
+    ''' 4 missing checks
+    w = [x for x in walls if x != 0]
+    for x in range(len(w) - 1):
+        c += min(w[x], w[x + 1])
+    '''
     return c
