bd323566633839bd202935687157fb0dcc67fe6f
Ian Culp <icculp@gmail.com>
2021-04-15 00:48:29 +0000

    First stab, simple solution

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Rain Interview task
+"""
+
+
+def rain(walls):
+    """ Given a list of non-negative integers representing walls of
+            width 1, calculate how much water will be retained
+            after it rains
+        walls is a list of non-negative integers
+        Returns: integer indicating total retained rainwater,
+            or 0 if list is empty
+    """
+    if len(walls) < 1:
+        return 0
+    w = [x for x in walls if x != 0]
+    c = 0
+    for x in range(len(w) - 1):
+        c += min(w[x], w[x + 1])
+    return c
