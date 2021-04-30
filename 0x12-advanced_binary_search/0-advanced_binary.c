356c4033c0b6524186c1c706a4d828bb6e47550a
Ian Culp <icculp@gmail.com>
2021-04-30 03:38:03 +0000

    Binary search printing correctly

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,6 +1,57 @@
 #include "search_algos.h"
 
 /**
+* printd - prints the subarray
+* @a: array to print
+* @l: left slice index
+* @r: right slice index
+*/
+
+void printd(int *a, int l, int r)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = l; i < r + 1; i++)
+	{
+		if (i < r)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+}
+
+/**
+* bin - binary recursive helper
+* @a: array to search
+* @l: left index
+* @r: right index
+* @v: Value to search for
+* @s: size of array
+* Return: index or -1 if not found
+*/
+
+int bin(int *a, int l, int r, int v, int s)
+{
+	int mid;
+
+	if (r >= l)
+	{
+		printd(a, l, r);
+		mid = l + (r - l) / 2;
+		if (a[mid] == v && a[mid - 1] != v)
+		{
+			return (mid);
+		}
+		if (a[mid] >= v)
+			return (bin(a, l, mid, v, s));
+		return (bin(a, mid + 1, r, v, s));
+	}
+
+	return (-1);
+}
+
+/**
 * advanced_binary - binary search returning first instance index
 * @array: pointer to first element of array to search in
 * @size: # of elements in array
@@ -10,7 +61,11 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
+	int i;
 
 
-	return (0);
+	if (array == NULL || size < 1)
+		return (-1);
+	i = bin(array, 0, (int)size - 1, value, (int)size);
+	return (i);
 }
