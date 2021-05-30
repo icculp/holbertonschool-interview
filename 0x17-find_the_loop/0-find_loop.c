647b2a28a745a6a934d89907652ab95ee71e3ef5
Ian Culp <icculp@gmail.com>
2021-05-30 21:48:36 +0000

    turtle and hare

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* find_listint_loop - find cycle in linked list
+* @head: head of list
+* Return: node of cycle or NULL
+*/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *turtle, *hare;
+
+	if (head == NULL)
+		return (NULL);
+	turtle = hare = head;
+	while (turtle && hare)
+	{
+		if (hare->next == turtle->next->next)
+			return (hare->next);
+		if (turtle->next)
+			turtle = turtle->next;
+		if (hare->next && hare->next->next)
+			hare = hare->next->next;
+		else
+			return (NULL);
+	}
+	return (NULL);
+}
