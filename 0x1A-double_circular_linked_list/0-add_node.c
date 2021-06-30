10275366d562805de70b56b507fa90d7ee70a728
Ian Culp <icculp@gmail.com>
2021-06-30 21:32:31 +0000

    Circular doubly linked list adders

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,64 @@
+#include "list.h"
+
+/**
+* add_node_end - adds node to end of dlist
+* @list: list to add to
+* @str: str to append to list
+* Return: ptr to node or null
+*/
+
+List *add_node_end(List **list, char *str)
+{
+	List *temp, *new;
+
+	if (list == NULL)
+		return (NULL);
+	new = malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+	new->str = str;
+	new->prev = new->next = new;
+	if (*list == NULL)
+	{
+		*list = new;
+		return (*list);
+	}
+	temp = new->next = *list;
+	(*list)->prev = new;
+	while (temp->next != NULL && temp->next != *list)
+		temp = temp->next;
+	temp->next = new;
+	new->prev = temp;
+	return (new);
+}
+
+/**
+* add_node_begin - adds node to begining of dlist
+* @list: list to add to
+* @str: str to append to list
+* Return: ptr to node or null
+*/
+
+List *add_node_begin(List **list, char *str)
+{
+	List *new;
+
+	if (list == NULL)
+		return (NULL);
+	new = malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+	new->str = str;
+	new->prev = new->next = new;
+	if (*list == NULL)
+	{
+		*list = new;
+		return (*list);
+	}
+	new->prev = (*list)->prev;
+	(*list)->prev->next = new;
+	(*list)->prev = new;
+	new->next = *list;
+	/***list = new;*/
+	return (new);
+}
