77bd5f81af4716ca6ef5f59f160532f2f824d481
Ian Culp <icculp@gmail.com>
2021-07-23 01:51:32 +0000

    binary_tree_node defined in header

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -27,7 +27,31 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
-void binary_tree_print(const binary_tree_t *tree)
+
+
+/**
+* binary_tree_node - creates a binary tree node
+* @parent: parent node pointer
+* @value: value to put in node
+* Return: pointer to new node, or NULL on failure
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+	new->n = value;
+	return (new);
+}
+
+
+void binary_tree_print(const binary_tree_t *tree);
 
 int bstr(binary_tree_t *tree, int min, int max);
 int binary_tree_is_bst(const binary_tree_t *tree);
