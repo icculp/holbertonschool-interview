932ab78a75942692894ffc6c15d68944b182df4a
Ian Culp <icculp@gmail.com>
2021-07-23 01:46:59 +0000

    prototypes in header

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -27,6 +27,12 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+int bstr(binary_tree_t *tree, int min, int max);
+int binary_tree_is_bst(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+bool avl(binary_tree_t *tree);
+int binary_tree_is_avl(const binary_tree_t *tree);
 
 
 #endif
