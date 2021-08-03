7a096d671bb25a21d9ce7f4664a7b8439e732a2d
Ian Culp <icculp@gmail.com>
2021-08-03 20:40:31 +0000

    pascals triangle

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    0-pascal_triangle.py
+"""
+
+
+def pascal_triangle(n):
+    """  returns a list of lists of integers representing
+            the Pascal’s triangle of n
+    """
+    ''' cool method '''
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        triangle.append([int(x) for x in str(11 ** i)])
+    return triangle
+
+''' lame method
+triangle = []
+for i in range(1, n + 1):
+    C = 1
+    row = []
+    for j in range(1, i + 1):
+        row.append(C)
+        C *= ((i - j) // j)
+    triangle.append(row)
+return triangle'''
