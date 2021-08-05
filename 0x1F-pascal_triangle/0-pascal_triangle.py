cbf1caf991333ff3b42083d43f423ff98467253b
Ian Culp <icculp@gmail.com>
2021-08-05 00:33:51 +0000

    powers of 11 doesn't work past n = 5

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -8,21 +8,22 @@ def pascal_triangle(n):
     """  returns a list of lists of integers representing
             the Pascal’s triangle of n
     """
-    ''' cool method '''
-    if n <= 0:
-        return []
+    ''' lame method '''
     triangle = []
-    for i in range(n):
-        triangle.append([int(x) for x in str(11 ** i)])
+    for i in range(1, n + 1):
+        C = 1
+        row = []
+        for j in range(1, i + 1):
+            row.append(C)
+            C = C * (i - j) // j
+        triangle.append(row)
     return triangle
 
-''' lame method
+
+''' cool method
+if n <= 0:
+    return []
 triangle = []
-for i in range(1, n + 1):
-    C = 1
-    row = []
-    for j in range(1, i + 1):
-        row.append(C)
-        C *= ((i - j) // j)
-    triangle.append(row)
+for i in range(n):
+    triangle.append([int(x) for x in str(11 ** i)])
 return triangle'''
