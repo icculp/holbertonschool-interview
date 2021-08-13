3e4042cb0b5410553c5e7bd9999f7fcacb508214
Ian Culp <icculp@gmail.com>
2021-08-13 00:43:16 +0000

    regex skeleton

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,18 @@
+#include "regex.h"
+
+/**
+* regex_match - determines if matches regex pattern in string
+* @str: string to find patter in, any ascii other than . or *
+* @pattern: Pattern to search in string (. or *)
+* Return: 1 if pattern matches, else 0
+*/
+
+int regex_match(char const *str, char const *pattern)
+{
+	int i;
+
+	(void)i;
+	(void)str;
+	(void)pattern;
+	return (1);
+}
