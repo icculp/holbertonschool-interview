05128292b6c70fd832ef445c33cc31bb132f4e56
Ian Culp <icculp@gmail.com>
2021-08-24 01:45:18 +0000

    prime game skeleton

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Prime game
+"""
+
+
+def isWinner(x, nums):
+    """ 
+        where x is the number of rounds and nums is an array of n
+        Return: name of the player that won the most rounds
+    """
+    return "Maria"
